434b138600283264956a4cdd13cdb3ea90538878
jopieji <jakeopie9@gmail.com>
2023-09-13 00:55:29 -0500

    initial commit

---
+++ src/main/java/com/example/greengroupecommerce/GreenGroupEcommerce.java
@@ -0,0 +1,14 @@
+package com.example.greengroupecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenGroupEcommerce {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GreenGroupEcommerce.class, args);
+
+    }
+}
