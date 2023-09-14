12529a46582d0096314896bddece0d7d4196a0fc
Jake Opie <72716605+jopieji@users.noreply.github.com>
2023-09-14 10:30:53 -0500

    add error page and links to icons (#7)

--- src/main/java/com/example/greengroupecommerce/controllers/UserController.java
+++ src/main/java/com/example/greengroupecommerce/controllers/UserController.java
@@ -1,16 +1,17 @@
 package com.example.greengroupecommerce.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-  public HomeController() {}
+public class UserController {
+  public UserController() {}
 
-  @GetMapping("/")
-    public String homePage(Model model) {
-      return "home";
+  @GetMapping("/user")
+    public String userPage(Model model) {
+      return "user";
     }
-    
+
 }
