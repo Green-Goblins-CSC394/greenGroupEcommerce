544a492c533309aa8c97d5d4db1a5de29157e994
ayum13 <andrewyum15@gmail.com>
2023-09-17 00:11:04 +0000

    checkout page

---
+++ src/main/java/com/example/greengroupecommerce/controllers/CheckoutController.java
@@ -0,0 +1,17 @@
+package com.example.greengroupecommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CheckoutController {
+  public CheckoutController() {
+  }
+
+  @GetMapping("/checkout")
+  public String checkoutPage(Model model) {
+    return "checkout";
+  }
+
+}
