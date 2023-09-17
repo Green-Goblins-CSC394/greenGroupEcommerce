a764b6343d9e3c9081de5d68fc14711418e054f5
ZahraAstrom <zahraabdo1@hotmail.com>
2023-09-17 06:05:08 +0000

    added confirmation, fixed checkout, added links

---
+++ src/main/java/com/example/greengroupecommerce/controllers/ConfirmationController.java
@@ -0,0 +1,17 @@
+package com.example.greengroupecommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConfirmationController {
+  public ConfirmationController() {
+  }
+
+  @GetMapping("/confirmation")
+  public String confirmationPage(Model model) {
+    return "confirmation";
+  }
+  
+}
