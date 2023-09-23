bfe39931b19ecd4122ec5ffb3aa07c2aefaa3b61
Jake Opie <72716605+jopieji@users.noreply.github.com>
2023-09-23 11:51:34 -0500

    refactor project structure and enable security config (#22)

--- src/main/java/com/greengroupecommerce/GreenGroupEcommerce.java
+++ src/main/java/com/greengroupecommerce/GreenGroupEcommerce.java
@@ -1,4 +1,4 @@
-package com.example.greengroupecommerce;
+package com.greengroupecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
