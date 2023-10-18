432c1b1fc569fc3405b5353b8e6ed20f1956f71a
Jake Opie <72716605+jopieji@users.noreply.github.com>
2023-10-18 13:02:50 -0500

    add stripe modules and configuration for external checkout (#35)

---
+++ src/main/java/com/greengroupecommerce/controllers/StripeController.java
@@ -0,0 +1,43 @@
+package com.greengroupecommerce.controllers;
+
+import com.stripe.Stripe;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+
+import lombok.Value;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/call-checkout")
+public class StripeController {
+
+    @PostMapping
+    public RedirectView createCheckoutSession() throws Exception {
+        @Value("${stripe.api.secretKey}")
+        final String secretKey;
+
+        Stripe.apiKey = secretKey;
+
+        String YOUR_DOMAIN = "http://localhost:8080";
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setSuccessUrl(YOUR_DOMAIN + "/confirmation")
+                        .setCancelUrl(YOUR_DOMAIN + "/home")
+                        .addLineItem(
+                                SessionCreateParams.LineItem.builder()
+                                        .setQuantity(1L)
+                                        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                                        .setPrice("price_1O2Po6AJzwVAVVDsohgTU5z1")
+                                        .build())
+                        .build();
+        Session session = Session.create(params);
+
+        return new RedirectView(session.getUrl());
+    }
+}
+
