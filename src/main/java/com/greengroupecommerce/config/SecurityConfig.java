108d17026a6c889f53de9d77a3b4d81afe713e57
jopieji <jakeopie9@gmail.com>
2023-10-18 03:43:22 +0000

    add stripe modules and configuration for external checkout

--- src/main/java/com/greengroupecommerce/config/SecurityConfig.java
+++ src/main/java/com/greengroupecommerce/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests( auth -> {
                 auth.requestMatchers("/user").authenticated();
                 auth.anyRequest().permitAll();
