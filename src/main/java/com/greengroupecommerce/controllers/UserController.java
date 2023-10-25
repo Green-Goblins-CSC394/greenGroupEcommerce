d0a12a0af887e56d88e66874a95ad2679bf533e0
ZahraAstrom <32182732+ZahraAstrom@users.noreply.github.com>
2023-10-24 20:53:14 -0500

    Added user page HTML and CSS (#56)
    Included removal button for cart

--- src/main/java/com/greengroupecommerce/controllers/UserController.java
+++ src/main/java/com/greengroupecommerce/controllers/UserController.java
@@ -1,17 +1,28 @@
 package com.greengroupecommerce.controllers;
 
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.greengroupecommerce.entities.Users;
+import com.greengroupecommerce.repositories.UsersRepository;
 
 @Controller
 public class UserController {
   public UserController() {}
 
+  @Autowired
+  private UsersRepository userRepository;
+
   @GetMapping("/user")
-    public String userPage(Model model) {
+    public String userPage(Principal principal, Model model) {
+      Users user = this.userRepository.findByEmail(principal.getName());
+      model.addAttribute("orderList", user.getOrders());
+      System.out.println(user.getOrders().size());
       return "user";
     }
-
 }
